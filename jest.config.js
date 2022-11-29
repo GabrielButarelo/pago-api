37c3949c6e4c66c03ac3cbd5cbd08f673d6d9990
Gabriel <gabriel.bonela@mistraltecnologia.com.br>
2022-11-29 12:08:46 -0300

    feat: update readme

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,15 @@
 /* eslint-disable no-undef */
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
-  },
+	roots: ['<rootDir>/src'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	coverageDirectory: 'coverage',
+	testEnvironment: 'node',
+	transform: {
+		'.+\\.ts$': 'ts-jest',
+	},
+	moduleNameMapper: {
+		'@modules/(.*)': '<rootDir>/src/modules/$1',
+		'@infra/(.*)': '<rootDir>/src/infra/$1',
+		'@shared/(.*)': '<rootDir>/src/shared/$1',
+	},
 };
